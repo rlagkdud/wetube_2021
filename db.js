ef89c9a71df53eb345674e0284200f9b1c17ded4
rlagkdud <ha0936861@gmail.com>
2021-02-18 14:43:44 +0900

    show home with mixin

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ export const videos=[
         title:"Video Awesome",
         description:"This is something I love",
         views:24,
-        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator:{
             id:121212,
             name:"hy",
@@ -16,7 +16,7 @@ export const videos=[
         title:"Video Nice",
         description:"This is something I love",
         views:24,
-        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator:{
             id:121212,
             name:"hy",
@@ -28,7 +28,7 @@ export const videos=[
         title:"Video Cool",
         description:"This is something I love",
         views:24,
-        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator:{
             id:121212,
             name:"hy",
@@ -40,7 +40,7 @@ export const videos=[
         title:"Video Perfect",
         description:"This is something I love",
         views:24,
-        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator:{
             id:121212,
             name:"hy",
