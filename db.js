0b30a69c5caf1e4b2f485b94ac21841ef7840bbf
rlagkdud <ha0936861@gmail.com>
2021-02-18 13:41:07 +0900

    show title, description at home

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos=[
+    {
+        id: 1234,
+        title:"Video Awesome",
+        description:"This is something I love",
+        views:24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator:{
+            id:121212,
+            name:"hy",
+            email:"asd@cd.com"
+        }
+    },
+    {
+        id: 1035,
+        title:"Video Nice",
+        description:"This is something I love",
+        views:24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator:{
+            id:121212,
+            name:"hy",
+            email:"asd@cd.com"
+        }
+    },
+    {
+        id: 1136,
+        title:"Video Cool",
+        description:"This is something I love",
+        views:24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator:{
+            id:121212,
+            name:"hy",
+            email:"asd@cd.com"
+        }
+    },
+    {
+        id: 123337,
+        title:"Video Perfect",
+        description:"This is something I love",
+        views:24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator:{
+            id:121212,
+            name:"hy",
+            email:"asd@cd.com"
+        }
+    }
+];
