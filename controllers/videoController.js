eb5e1b2b41da4e3f8f096681fe34722ae7a7a36b
rlagkdud <ha0936861@gmail.com>
2021-02-19 17:48:25 +0900

    controllers

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,6 @@
 import {videos} from "../db";
+import routes from "../routes";
+
 export const home = (req,res) => {
     res.render("home",{ pageTitle:"Home", videos });
 }
@@ -8,7 +10,15 @@ export const search = (req,res) => {
     res.render("search",{ pageTitle:"Search", searchingBy,videos });
 };
 
-export const upload = (req,res) => res.render("upload",{ pageTitle:"Upload" });
+export const getUpload = (req,res) => res.render("upload",{ pageTitle:"Upload" });
+export const postUpload = (req, res) => {
+    const {
+        body:{file,title,description}
+    } = req;
+    //To Do: upload and save video
+    res.redirect(routes.videoDetail(1234));
+}
+  
 
 export const videoDetail = (req,res) => res.render("videoDetail",{ pageTitle:"Video Detail" });
 
